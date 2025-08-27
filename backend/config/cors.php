a314f2c90f361f41054f4c05dcb5d0937e9e1139
LinkAnJarad <linkanjarad@yahoo.com>
2025-08-27 19:13:38 +0800

    added https

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['*'], //['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],//['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173', 'http://192.168.254.118:5173'],
     'allowed_origins_patterns' => [],
